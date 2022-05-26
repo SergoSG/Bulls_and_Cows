f01699b2bb64af5ae368d3b330c35063ea8bc8a3
Sergey Ilyushkin <ilushkinss@gmail.com>
2022-05-26 16:17:09 +0300

    -

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -5,21 +5,53 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String input;
 
         String secretNum;
         int turnCnt = 0;
 
         do {
+            // 1
             System.out.println("Input the length of the secret code:");
-            int secretNumLength = scanner.nextInt();
+            int secretNumLength;
+            input = scanner.nextLine();
+            if (isNotDigit(input)) {
+                System.out.printf("Error: \"%s\" isn't a valid number.\n", input);
+                return;
+            } else {
+                secretNumLength = Integer.parseInt(input);
+            }
+            if (secretNumLength < 1 || secretNumLength > 36) {
+                System.out.printf("Error: can't generate a secret number with a length of %d because there aren't enough " +
+                        "unique digits.\n", secretNumLength);
+                return;
+            }
 
+            // 2
             System.out.println("Input the number of possible symbols in the code:");
-            int secretNumSymbolCnt = scanner.nextInt();
+            int secretNumSymbolCnt;
+            input = scanner.nextLine();
+            if (isNotDigit(input)) {
+                System.out.printf("Error: \"%s\" isn't a valid number.\n", input);
+                return;
+            } else {
+                secretNumSymbolCnt = Integer.parseInt(input);
+            }
+            if (secretNumSymbolCnt > 36) {
+                System.out.printf("Error: maximum number of possible symbols in the code is 36 (0-9, a-z).\n");
+                return;
+            }
+            if (secretNumSymbolCnt < secretNumLength) {
+                System.out.printf("Error: it's not possible to generate a code with a length of %s with %s unique symbols.\n",
+                        secretNumLength,
+                        secretNumSymbolCnt);
+                return;
+            }
 
             secretNum = generateRandom(secretNumLength, secretNumSymbolCnt);
         } while (secretNum == null || secretNum.isEmpty());
         System.out.println("Okay, let's start a game!");
-        scanner.nextLine();
+        //scanner.nextLine();
 
         String enterNum;
         do {
@@ -27,6 +59,18 @@ public class Main {
             System.out.printf("Turn %d:\n", turnCnt);
             enterNum = scanner.nextLine();
         } while (!checkNum(secretNum, enterNum));
+    }
+
+    static boolean isNotDigit(String str) {
+        if (str.length() == 0) {
+            return true;
+        }
+        try {
+            int num = Integer.parseInt(str);
+            return false;
+        } catch (NumberFormatException e) {
+            return true;
+        }
     }
 
     static String generateRandom(int secretNumLength, int secretNumSymbolCnt) {
@@ -73,7 +117,7 @@ public class Main {
         if (bulls == 0 && cows == 0) {
             System.out.println("Grade: 0 bull(s) or cow(s)");
         } else {
-            System.out.printf("Grade: %s%s%s%s%s\n",
+            System.out.printf("Grade: %s%s%s%s%s%s\n",
                     bulls == 0 ? "" : String.valueOf(bulls),
                     bulls == 0 ? "" : " bull(s)",
                     bulls != 0 && cows != 0 ? " and " : "",
--- Topics/Recursion/Number of decompositions/src/Main.java
+++ Topics/Recursion/Number of decompositions/src/Main.java
@@ -1,5 +1,82 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int enter = Integer.parseInt(scanner.nextLine());
+
+        //ArrayList<Integer> array = new ArrayList<>(Arrays.asList(enter));
+        //decompose1(array, enter);
+        decompose(enter);
+        System.out.print("\n" + enter);
+    }
+
+    public static void decompose (int enter) {
+        if (enter == 1) {
+            System.out.print(enter + " ");
+            return;
+        } else {
+            int num1 = enter - 1;
+            int num2 = 1;
+            int cnt = 1;
+            while (num1 >= num2) {
+                decompose(num1);
+                decompose(num2);
+                if (num1 > 1) {
+                    System.out.print("\n" + num1 + " " + num2);
+                }
+                cnt++;
+                num1 = enter - cnt;
+                num2 = enter - num1;
+            }
+        }
+    }
+
+
+
+    public static void decompose1 (ArrayList<Integer> array, int enter) {
+        if (array.size() == enter) {
+            return;
+        }
+
+        if (array.get(array.size() - 1) > 1) {
+            ArrayList<Integer> tmpArray = new ArrayList<>();
+            if (array.size() > 1) {
+                tmpArray.addAll(0, array.subList(0, array.size() - 2));
+            }
+            tmpArray.add(array.get(array.size() - 1) - 1);
+            tmpArray.add(1);
+
+            for (int i = 0; i < tmpArray.size(); i++) {
+                System.out.print(tmpArray.get(i) + " ");
+            }
+            System.out.println();
+        } else {
+
+        }
+/*
+        if (array.size() == 1) {
+            System.out.println(array.get(0));
+        } else {
+            for (int i = 0; i < array.size(); i++) {
+                System.out.print(array.get(i) + " ");
+            }
+            System.out.println();
+
+            for (int i = 0; i < array.size() / 2; i++) {
+                ArrayList<Integer> tmpArray = new ArrayList<>();
+                tmpArray.add(i, array.get(i * 2) + array.get(i * 2 + 1));
+                tmpArray.addAll(i + 1, array.subList(i * 2 + 1, array.size() - 1));
+                //decompose(tmpArray);
+                for (int j = 0; j < tmpArray.size(); j++) {
+                    System.out.print(tmpArray.get(j) + " ");
+                }
+                System.out.println();
+            }
+        }
+
+ */
     }
 }
